7d24cc6d9672ee7f16d62aae3b0f88c673a0e7da
HankBedford <Zachary.Lynch1337@gmail.com>
2018-11-04 08:51:34 -0800

    Getting data from Zomato API with hard coded location

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
   // api url https://developers.zomato.com/api/v2.1/search?count=3&lat=36.0144&lon=-115.1174&radius=75&sort=real_distance&order=asc
 
+ 
+  componentDidMount() {
+    this.api = '30b531a86dd28c712d12265fbd94fafa';
+    this.header = {
+      method: "GET",
+      headers: {
+        "user-key": this.api,
+        "Content-type": "application/json"
+      },
+      credentails: "same-origin"
+    }
+    fetch('https://developers.zomato.com/api/v2.1/search?count=3&lat=36.0144&lon=-115.1174&radius=75&sort=real_distance&order=asc', this.header)
+    .then(result => result.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    } );
+
+  }
+
   render() {
+    const { isLoaded, items } = this.state;
+    console.log(items);
+    if (isLoaded){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Loaded
           </p>
           <a
             className="App-link"
@@ -24,7 +56,11 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    );} else {
+      return (
+        <div>Didn't load</div>
+      );
+    }
   }
 }
 
